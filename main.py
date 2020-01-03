a347eca459aece4ce17612e9df7885751a48b26d
keethesh <keethesh15@Gmail.com>
2020-01-03 11:45:24 +0400

    Big big changes to program execution, bug fixes, added a progress bar

--- main.py
+++ main.py
@@ -6,24 +6,30 @@ import pyperclip
 from functions import *
 
 udemy_url = input("Paste in your Udemy course URL: ")
+print("")
+print("Search starting...")
+print("")
 # headless = input("Do you want to run the browser in headless mode? (Y/n): ").lower()
 # if not headless == ("y" or "n"):
 #     headless = "y"
 course_name = get_course_name(udemy_url)
-course_info = get_info(udemy_url)
+course_info = get_sites(course_name)
 if len(course_info) == 0:
     exit("The course wasn't found in any of the sharing websites.")
 for i in range(3):
     print("")
 download_link = course_info[0].get("link")
-if "magnet" in download_link:
-    download_link = findall("magnet:?xt=urn:btih:[a-zA-Z0-9]*", download_link)
+try:
+    if "magnet" in download_link:
+        download_link = findall("magnet:?xt=urn:btih:[a-zA-Z0-9]*", download_link)
+except TypeError:
+    pass
 print("The Udemy course \"" + course_name + "\" can be downloaded at " + download_link)
 print("It has last been updated on " + str(course_info[0]["month"]) + "/" + str(course_info[0]["year"]) + ".")
-copy_to_clipboard = input("Do you want me to copy the link to your clipboard? (Y/n): ").lower()
-if not copy_to_clipboard == ("y" or "n"):
-    copy_to_clipboard = "y"
-if copy_to_clipboard == "y":
+copy_to_clipboard = input("Do you want me to copy the link to your clipboard? (Y/n): ").upper()
+if not copy_to_clipboard == "Y" or copy_to_clipboard == "N":
+    copy_to_clipboard = "Y"
+if copy_to_clipboard == "Y":
     pyperclip.copy(download_link)
     print("")
     print("The link has successfully been copied to clipboard.")
